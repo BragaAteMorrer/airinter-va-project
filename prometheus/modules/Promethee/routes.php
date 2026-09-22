dcbe21d143af1951cecb75d00f4b2b914d3c243a
BragaAteMorrer <96670909+BragaAteMorrer@users.noreply.github.com>
2026-09-22 22:48:15 +0200

    feat(API): exposer SimBrief sous le contrat opérations v1

--- prometheus/modules/Promethee/routes.php
+++ prometheus/modules/Promethee/routes.php
@@ -7,6 +7,7 @@ use Modules\Promethee\Http\AcarsOperationsController;
 use Modules\Promethee\Http\AcarsConfigurationController;
 use Modules\Promethee\Http\OperationsV1Controller;
 use Modules\Promethee\Http\HermesReleaseController;
+use App\Http\Controllers\Api\AcarsSimBriefController;
 
 // Browsers request this conventional path even though the branded icon lives
 // with the static Promethee assets.
@@ -165,6 +166,10 @@ Route::middleware(['api','api.auth'])->prefix('api/v1')->group(function () {
     Route::get('/operations/{bid}/pirep', [OperationsV1Controller::class, 'pirep']);
     Route::post('/operations/{bid}/pirep', [OperationsV1Controller::class, 'prefilePirep']);
     Route::post('/operations/{bid}/telemetry', [TelemetryController::class, 'storeOperation']);
+    Route::post('/operations/{operation}/simbrief/session', [AcarsSimBriefController::class, 'sessionOperation']);
+    Route::post('/operations/{operation}/simbrief/redirect', [AcarsSimBriefController::class, 'redirectOperation']);
+    Route::post('/operations/{operation}/simbrief/account/import', [AcarsSimBriefController::class, 'importAccountOperation']);
+    Route::post('/operations/{operation}/simbrief/import', [AcarsSimBriefController::class, 'importOperation']);
     Route::get('/hermes/configuration', [AcarsConfigurationController::class, 'show']);
 });
 
