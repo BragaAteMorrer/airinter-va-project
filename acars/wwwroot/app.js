564d08f034b506d494b8b722b49ec37c2238dfc3
BragaAteMorrer <96670909+BragaAteMorrer@users.noreply.github.com>
2026-09-23 18:12:47 +0200

    Hermes: remove server endpoint details from pilot UI

--- acars/wwwroot/app.js
+++ acars/wwwroot/app.js
@@ -1029,13 +1029,7 @@ refreshStatus();
 setInterval(refreshStatus, 1000);
 call('/api/about').then(info => {
   setText($('#build'), 'Version ' + info.version);
-  setText($('#loginServer'), info.server || 'Serveur inconnu');
-  setText($('#loginEndpoint'), info.loginEndpoint ? 'POST ' + info.loginEndpoint : '');
-  setText($('#loginServerSource'), info.serverSource ? 'Source : ' + info.serverSource : '');
-}).catch(error => {
-  setText($('#loginServer'), 'Impossible de lire la configuration Hermès');
-  setText($('#loginEndpoint'), error.message || '');
-});
+}).catch(() => setText($('#build'), 'Version inconnue'));
 
 
 const checkUpdateBtn = $('#checkUpdateBtn');
