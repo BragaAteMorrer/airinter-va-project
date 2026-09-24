082bdea1ac95fc70d174f7892de61e3fb5a0418d
BragaAteMorrer <96670909+BragaAteMorrer@users.noreply.github.com>
2026-09-24 21:53:04 +0200

    feat(promethee): expose SimBrief admin routes

--- prometheus/modules/Promethee/routes.php
+++ prometheus/modules/Promethee/routes.php
@@ -88,6 +88,8 @@ Route::middleware(['web','auth','ability:admin,admin-access'])->prefix('admin/pr
         Route::get('/identite', [PortalController::class, 'branding'])->name('branding');
         Route::post('/identite', [PortalController::class, 'saveBranding'])->name('branding.save');
         Route::post('/identite/importer', [PortalController::class, 'importBranding'])->name('branding.import');
+        Route::get('/simbrief', [PortalController::class, 'adminSimbrief'])->name('simbrief');
+        Route::post('/simbrief/sync', [PortalController::class, 'syncSimbrief'])->name('simbrief.sync');
         Route::post('/calendar', [PortalController::class,'saveEvent'])->name('calendar.save');
         Route::delete('/calendar/{id}', [PortalController::class,'deleteEvent'])->name('calendar.delete');
         Route::post('/pilots/{id}', [PortalController::class,'saveMember'])->name('pilots.save');
