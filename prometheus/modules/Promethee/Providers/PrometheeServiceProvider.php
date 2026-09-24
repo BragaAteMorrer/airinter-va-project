807402ae07b80fbc796a587aa8f5ae12f4081a5c
BragaAteMorrer <96670909+BragaAteMorrer@users.noreply.github.com>
2026-09-25 00:34:23 +0200

    feat(promethee): add SOP admin navigation

--- prometheus/modules/Promethee/Providers/PrometheeServiceProvider.php
+++ prometheus/modules/Promethee/Providers/PrometheeServiceProvider.php
@@ -34,5 +34,6 @@ class PrometheeServiceProvider extends ServiceProvider
         app(ModuleService::class)->addFrontendLink('Prométhée', '/', 'fas fa-plane', true);
         app(ModuleService::class)->addAdminLink('Prométhée · Identité', '/admin/promethee/identite', 'pe-7s-photo');
         app(ModuleService::class)->addAdminLink('Prométhée · SimBrief', '/admin/promethee/simbrief', 'pe-7s-plane');
+        app(ModuleService::class)->addAdminLink('Prométhée · SOP', '/admin/promethee/sop', 'pe-7s-shield');
     }
 }
