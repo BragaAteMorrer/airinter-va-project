ab57b07309d90cfde15dabc5b72e64a565c493c9
BragaAteMorrer <96670909+BragaAteMorrer@users.noreply.github.com>
2026-09-25 00:37:46 +0200

    test(hermes): acknowledge SOP facts before completion

--- acars.tests/ReferenceFlightE2ETests.cs
+++ acars.tests/ReferenceFlightE2ETests.cs
@@ -112,6 +112,7 @@ public sealed class ReferenceFlightE2ETests
 
         recorder.AcknowledgePositions(recorder.Pending.Select(x => x.Sample.SampleId).ToArray());
         recorder.AcknowledgeEvents(recorder.PendingEvents.Select(x => x.EventId).ToArray());
+        recorder.AcknowledgeFacts(recorder.PendingFacts.Select(x => x.FactId).ToArray());
         recorder.Complete();
         Assert.Null(recorder.Flight);
     }
