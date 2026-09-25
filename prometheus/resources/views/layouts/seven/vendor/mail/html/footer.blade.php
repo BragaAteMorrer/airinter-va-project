6f3280aa409d7a08fe7fc0c831b530b542ceb1c8
BragaAteMorrer <96670909+BragaAteMorrer@users.noreply.github.com>
2026-09-25 02:53:27 +0200

    feat(mail): add Air Inter branded footer

--- prometheus/resources/views/layouts/seven/vendor/mail/html/footer.blade.php
+++ prometheus/resources/views/layouts/seven/vendor/mail/html/footer.blade.php
@@ -1,9 +1,10 @@
 <tr>
   <td>
-    <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation">
+    <table class="footer" align="center" width="600" cellpadding="0" cellspacing="0" role="presentation">
       <tr>
         <td class="content-cell" align="center">
-          {{ Illuminate\Mail\Markdown::parse($slot) }}
+          <p><strong>AIR INTER · PROMÉTHÉE</strong><br>Pourquoi vivre sans ailes !</p>
+          <p>{{ Illuminate\Mail\Markdown::parse($slot) }}</p>
         </td>
       </tr>
     </table>
