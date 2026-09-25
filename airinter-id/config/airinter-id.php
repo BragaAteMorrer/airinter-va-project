dae9f2d2a486aa8d290acd1ee8739a9fe1b47506
BragaAteMorrer <96670909+BragaAteMorrer@users.noreply.github.com>
2026-09-25 15:34:10 +0200

    feat(identity): define first-party OAuth clients

--- airinter-id/config/airinter-id.php
+++ airinter-id/config/airinter-id.php
@@ -8,4 +8,21 @@ return [
         'pilot_id_prefix' => env('PROMETHEE_PILOT_ID_PREFIX', 'IT'),
         'pilot_id_length' => (int) env('PROMETHEE_PILOT_ID_LENGTH', 3),
     ],
+    'clients' => [
+        'promethee' => [
+            'name' => 'Prométhée',
+            'redirect_uri' => env('AIRINTER_ID_PROMETHEE_CALLBACK', 'https://promethee.airinter-va.org/auth/airinter-id/callback'),
+            'confidential' => true,
+        ],
+        'hermes' => [
+            'name' => 'Hermès',
+            'redirect_uri' => env('AIRINTER_ID_HERMES_CALLBACK', 'http://127.0.0.1:47821/callback'),
+            'confidential' => false,
+        ],
+        'website' => [
+            'name' => 'Air Inter VA',
+            'redirect_uri' => env('AIRINTER_ID_PUBLIC_CALLBACK', 'https://www.airinter-va.org/auth/airinter-id/callback'),
+            'confidential' => true,
+        ],
+    ],
 ];
