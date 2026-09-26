e91b2ff5fe8ebc40b1a9ab33e50c29bb993b0f76
BragaAteMorrer <96670909+BragaAteMorrer@users.noreply.github.com>
2026-09-26 03:21:05 +0200

    feat(hermes): define compatible simulator aircraft variants

--- prometheus/modules/Promethee/Config/acars.php
+++ prometheus/modules/Promethee/Config/acars.php
@@ -31,5 +31,39 @@ return [
         'a300-b2' => ['msfs2020' => ['addon' => 'iniBuilds A300-600R', 'simbrief_type' => 'A306'], 'msfs2024' => ['addon' => 'iniBuilds A300-600R', 'simbrief_type' => 'A306']],
         'a300-b4' => ['msfs2020' => ['addon' => 'iniBuilds A300-600R', 'simbrief_type' => 'A306'], 'msfs2024' => ['addon' => 'iniBuilds A300-600R', 'simbrief_type' => 'A306']],
         'dc-8-63pf' => ['msfs2020' => ['addon' => 'Aeroplane Heaven 707-320', 'simbrief_type' => 'B703'], 'msfs2024' => ['addon' => 'Aeroplane Heaven 707-320', 'simbrief_type' => 'B703']],
-    ],
+    ],,
+    /*
+     * Simulator variants are separate from the physical phpVMS fleet.
+     * A pilot may fly the assigned Air Inter aircraft with any compatible
+     * add-on below. The selected variant drives SimBrief performance and lets
+     * Hermès compare the expected add-on with the simulator adapter it detects.
+     */
+    'variants' => [
+        'A319' => [
+            ['id' => 'generic-a319', 'label' => 'Airbus A319 (générique)', 'vendor' => 'Generic', 'simbrief_type' => 'A319', 'simulators' => ['msfs2020','msfs2024','fsx','p3d','xplane'], 'adapter_ids' => [], 'default' => true],
+            ['id' => 'fenix-a319', 'label' => 'Fenix A319', 'vendor' => 'Fenix Simulations', 'simbrief_type' => 'A319', 'simulators' => ['msfs2020','msfs2024'], 'adapter_ids' => ['fenix-a319']],
+        ],
+        'A320' => [
+            ['id' => 'generic-a320', 'label' => 'Airbus A320 (générique)', 'vendor' => 'Generic', 'simbrief_type' => 'A320', 'simulators' => ['msfs2020','msfs2024','fsx','p3d','xplane'], 'adapter_ids' => [], 'default' => true],
+            ['id' => 'fenix-a320', 'label' => 'Fenix A320', 'vendor' => 'Fenix Simulations', 'simbrief_type' => 'A320', 'simulators' => ['msfs2020','msfs2024'], 'adapter_ids' => ['fenix-a320']],
+            ['id' => 'fbw-a32nx', 'label' => 'FlyByWire A32NX', 'vendor' => 'FlyByWire', 'simbrief_type' => 'A20N', 'simulators' => ['msfs2020','msfs2024'], 'adapter_ids' => ['fbw-a32nx']],
+            ['id' => 'inibuilds-a320neo', 'label' => 'iniBuilds A320neo', 'vendor' => 'iniBuilds', 'simbrief_type' => 'A20N', 'simulators' => ['msfs2024'], 'adapter_ids' => ['inibuilds-a320']],
+        ],
+        'A321' => [
+            ['id' => 'generic-a321', 'label' => 'Airbus A321 (générique)', 'vendor' => 'Generic', 'simbrief_type' => 'A321', 'simulators' => ['msfs2020','msfs2024','fsx','p3d','xplane'], 'adapter_ids' => [], 'default' => true],
+            ['id' => 'fenix-a321', 'label' => 'Fenix A321', 'vendor' => 'Fenix Simulations', 'simbrief_type' => 'A321', 'simulators' => ['msfs2020','msfs2024'], 'adapter_ids' => ['fenix-a321']],
+        ],
+        'A310' => [
+            ['id' => 'generic-a310', 'label' => 'Airbus A310 (générique)', 'vendor' => 'Generic', 'simbrief_type' => 'A310', 'simulators' => ['msfs2020','msfs2024','xplane'], 'adapter_ids' => [], 'default' => true],
+            ['id' => 'inibuilds-a310', 'label' => 'iniBuilds A310-300', 'vendor' => 'iniBuilds', 'simbrief_type' => 'A310', 'simulators' => ['msfs2020','msfs2024'], 'adapter_ids' => ['inibuilds-a310']],
+        ],
+        'B737' => [
+            ['id' => 'generic-b737', 'label' => 'Boeing 737 (générique)', 'vendor' => 'Generic', 'simbrief_type' => 'B738', 'simulators' => ['msfs2020','msfs2024','fsx','p3d','xplane'], 'adapter_ids' => [], 'default' => true],
+            ['id' => 'pmdg-b737', 'label' => 'PMDG 737', 'vendor' => 'PMDG', 'simbrief_type' => 'B738', 'simulators' => ['msfs2020','msfs2024'], 'adapter_ids' => ['pmdg']],
+        ],
+        'MD11' => [
+            ['id' => 'generic-md11', 'label' => 'McDonnell Douglas MD-11 (générique)', 'vendor' => 'Generic', 'simbrief_type' => 'MD11', 'simulators' => ['msfs2020','msfs2024','xplane'], 'adapter_ids' => [], 'default' => true],
+            ['id' => 'tfdi-md11', 'label' => 'TFDi MD-11', 'vendor' => 'TFDi Design', 'simbrief_type' => 'MD11', 'simulators' => ['msfs2020','msfs2024'], 'adapter_ids' => ['tfdi-md11']],
+        ],
+    ]
 ];
